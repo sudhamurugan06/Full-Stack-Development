eaffe436871db9c0b74697ab5225ebef02122b83
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-04-29 20:07:16 +0530

    Create VowelCount.js

---
+++ Javascript/VowelCount.js
@@ -0,0 +1,10 @@
+var a="javascript";
+var cnt=0;
+for(let i=0;i<a.length;i++)
+{
+    if(a[i]=='a' || a[i]=='e' || a[i]=='i' || a[i]=='o' || a[i]=='u')
+    {
+        cnt++;
+    }
+}
+console.log(cnt);
