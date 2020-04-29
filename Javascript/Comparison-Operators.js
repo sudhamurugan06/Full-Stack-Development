a1ba903bd816c5d39be3d8f3de5633d1006b510e
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-04-29 19:07:54 +0530

    Create Comparison-Operators.js

---
+++ Javascript/Comparison-Operators.js
@@ -0,0 +1,23 @@
+var x = 25;
+    var y = 35;
+    var z = "25";
+     
+    console.log(x == z);  
+    
+    
+    console.log(x === z); 
+    
+    
+    console.log(x != y);  
+   
+    
+    console.log(x !== z); 
+   
+    
+    console.log(x < y);   
+  
+    
+    console.log(x > y);  
+    
+    
+    console.log(x <= y); 
